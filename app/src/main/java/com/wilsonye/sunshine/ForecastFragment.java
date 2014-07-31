ddbb01a0d6ea43c0eba6c5a569222e19721a0dcf
LqYe <liqiang.w.ye@gmail.com>
2014-07-30 22:16:35 -0500

    Added New Classes

--- app/src/main/java/com/wilsonye/sunshine/ForecastFragment.java
+++ app/src/main/java/com/wilsonye/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.wilsonye.sunshine;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -86,6 +89,14 @@ import java.util.List;
 
             ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
             listView.setAdapter(mForecastAdapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    String forecast = mForecastAdapter.getItem(i);
+                    Toast.makeText(getActivity(),forecast, Toast.LENGTH_SHORT).show();
+
+                }
+            });
 
             return rootView;
         }
