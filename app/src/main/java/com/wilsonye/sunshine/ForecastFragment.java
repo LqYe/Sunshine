7c782f0e67a51b6eb4677b0d680ff44721744feb
LqYe <liqiang.w.ye@gmail.com>
2014-08-03 23:57:19 -0500

    Added Location Setting

--- app/src/main/java/com/wilsonye/sunshine/ForecastFragment.java
+++ app/src/main/java/com/wilsonye/sunshine/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.wilsonye.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -29,9 +31,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
 /**
      * A placeholder fragment containing a simple view for week weather forecast.
@@ -58,12 +58,26 @@ import java.util.List;
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
             if (id == R.id.action_refresh) {
-                FetchWeatherTask weatherTask = new FetchWeatherTask();
-                weatherTask.execute("60657");
+                updateWeather();
                 return true;
             }
 
             return super.onOptionsItemSelected(item);
+        }
+
+        private void updateWeather() {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            SharedPreferences sharedPref = PreferenceManager.
+                    getDefaultSharedPreferences(getActivity());
+            String location = sharedPref.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+            weatherTask.execute(location);
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            updateWeather();
         }
 
         @Override
@@ -71,21 +85,10 @@ import java.util.List;
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-            String[] forecastArray = {
-                    "Today - Sunny - 88/63",
-                    "Tomorrow - Foggy - 70/40",
-                    "Weds - Cloudy - 72/63",
-                    "Thurs - Asteroids - 75/65",
-                    "Fri - Heavy Rain - 65/56",
-                    "Sat - Gloomy - 60/51",
-                    "Sun - Sunny - 80/68"
-            };
-
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
             mForecastAdapter = new ArrayAdapter<String>(getActivity(),
                     R.layout.list_item_forecast,
                     R.id.list_item_forecast_textview,
-                    weekForecast);
+                    new ArrayList<String>());
 
             ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
             listView.setAdapter(mForecastAdapter);
